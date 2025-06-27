7fea7b45f7168c42061b5c06c61860473cb36f54
maria sidorova <sidmashav@icloud.com>
2025-06-27 12:18:20 +0100

    Set Up Light and Dark Theme - Shadcn/UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,51 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import ThemeProvider from "@/context/Theme";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = Inter({
-    variable: "--font-inter",
-    weight: ["100","200","300","400","500","600","700","800","900"],
-    subsets: ["latin"],
-    display: "swap",
-    fallback: ["system-ui", "sans-serif"],
+	variable: "--font-inter",
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	subsets: ["latin"],
+	display: "swap",
+	fallback: ["system-ui", "sans-serif"],
 });
 const spaceGrotesk = Space_Grotesk({
-    variable: "--font-space-grotesk",
-    weight: ["300","400","500","600","700"],
-    subsets: ["latin"],
-    display: "swap",
-    fallback: ["system-ui", "sans-serif"],
+	variable: "--font-space-grotesk",
+	weight: ["300", "400", "500", "600", "700"],
+	subsets: ["latin"],
+	display: "swap",
+	fallback: ["system-ui", "sans-serif"],
 });
 
-
 export const metadata: Metadata = {
-  title: "DevFlow",
-  description: "A comunity-driven platform for asking and answering programming questions. Get help, share knowledge and collaborate with developers from around the world. Explore topics in web developemnt, mobile app development, algorithms, data structures and more. Become a part of the DevFlow community today!",
-  icons:{
-    icon:'/images/site-logo.svg'
-  }
+	title: "DevFlow",
+	description:
+		"A comunity-driven platform for asking and answering programming questions. Get help, share knowledge and collaborate with developers from around the world. Explore topics in web developemnt, mobile app development, algorithms, data structures and more. Become a part of the DevFlow community today!",
+	icons: {
+		icon: "/images/site-logo.svg",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem={true}
+					disableTransitionOnChange={true}
+				>
+					<Navbar />
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-
 export default function Home() {
 	return (
 		<div className="">
 			<h1 className="h1-bold"> Welcome to the world on Next.js</h1>
-            <h1 className="h1-bold font-space-grotesk"> Welcome to the world on Next.js</h1>
 		</div>
 	);
-
 }
