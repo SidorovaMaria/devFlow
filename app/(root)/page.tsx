14de3f94fc69376c37c13703258c0f18718114a9
maria sidorova <sidmashav@icloud.com>
2025-06-29 17:56:53 +0100

    Home Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,25 @@
-import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
 const Home = async () => {
-	const session = await auth();
-	console.log("Session:", session);
 	return (
-		<div className="">
-			<h1 className="h1-bold"> Welcome to the world on Next.js</h1>
-		</div>
+		<>
+			<section className="w-full flex flex-col-reverse justify-between gap-4 sm:flex-row  sm:items-center">
+				<h1 className="h1-bold text-dark100_light900"> All Questions</h1>
+				<Button className="primary-gradient min-h-[46px] px-4 py-3 text-light-900!" asChild>
+					<Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+				</Button>
+			</section>
+			<section className="mt-11">Local Seacrh</section>
+			HomeFilter
+			<div className="mt-11  flex w-full flex-col gap-6">
+				<p>Questuion Card 1</p>
+				<p>Questuion Card 1</p>
+				<p>Questuion Card 1</p>
+				<p>Questuion Card 1</p>
+			</div>
+		</>
 	);
 };
 export default Home;
