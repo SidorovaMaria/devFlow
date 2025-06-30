3155211a0905359f202d688351b0376cab533a49
maria sidorova <sidmashav@icloud.com>
2025-06-30 13:26:58 +0100

    Tag and Vote schemas

---
+++ database/tag.model.ts
@@ -0,0 +1,17 @@
+import { model, models, Schema } from "mongoose";
+
+interface ITag {
+	name: string;
+	question: number;
+}
+const TagSchema = new Schema<ITag>(
+	{
+		name: { type: String, required: true, unique: true },
+		question: { type: Number, default: 0 },
+	},
+	{
+		timestamps: true,
+	}
+);
+const Tag = models?.Tag || model<ITag>("Tag", TagSchema);
+export default Tag;
