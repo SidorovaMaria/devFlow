d5832a1a09852fec02c58c0fea69c4d8f64b31cf
maria sidorova <sidmashav@icloud.com>
2025-06-30 13:15:59 +0100

    More Models for Mongoose and MongoDB

--- database/account.model.ts
+++ database/account.model.ts
@@ -8,14 +8,19 @@ interface IAccount {
 	provider: string;
 	providerAccountId: string;
 }
-const AccounSchema = new Schema<IAccount>({
-	userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-	name: { type: String, required: true },
-	image: { type: String },
-	password: { type: String },
-	provider: { type: String, required: true },
-	providerAccountId: { type: String, required: true },
-});
+const AccounSchema = new Schema<IAccount>(
+	{
+		userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+		name: { type: String, required: true },
+		image: { type: String },
+		password: { type: String },
+		provider: { type: String, required: true },
+		providerAccountId: { type: String, required: true },
+	},
+	{
+		timestamps: true,
+	}
+);
 
 const Account = models?.Account || model<IAccount>("Account", AccounSchema);
 export default Account;
